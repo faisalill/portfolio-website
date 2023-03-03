a97e69bb149df4ab2c6a956be76f5d594b5cd413
faisalill <mfaisal.ilyas.d@gmail.com>
2023-03-03 13:48:11 +0530

    first page to second transition complete

--- index.js
+++ index.js
@@ -30,6 +30,39 @@ tl.to(".container",{
     rotationY: "20deg",
     rotationZ: "5deg",
     scale: 0.5,
-    y: 190,
+    y: "40%",
     duration: 2,
+})
+tl.to(".container-2", {
+    x: "50%",
+    y: "42%",
+    duration: 2,
+}, "-=2")
+tl.to(".container", {
+    x:"-50%",
+    duration: 1,
+})
+tl.to(".container-2", {
+    x: "0%",
+    // y: "50%",
+    duration: 1,
+}, "-=1")
+tl.to(".container-2", {
+    y: "50%",
+    duration: 0.4,
+})
+tl.to(".container-2", {
+    scale: 1,
+    rotationX: "0deg",
+    rotationY: "0deg",
+    rotationZ: "0deg",
+    y: "180%",
+    duration: 0.4,
+})
+tl.to(".container-2", {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    duration: 1,
 })
